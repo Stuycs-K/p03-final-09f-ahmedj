4df4e14e9346be0ba6297cf5806138e0ba8d45e7
jidanahmed <jidanoutbox@gmail.com>
2026-01-07 13:50:27 -0600

    started map system

--- server.c
+++ server.c
@@ -1,13 +1,32 @@
 #include "networking.h"
 
+void getMap(char * path, char map[][] *) {
+  // read mapPath into buff
+  char buff[BUFFER_SIZE];
+  FILE * mapFD = fopen(path, "r");
+  fgets(buff, sizeof(buff); mapFD)
+
+  // get width and height of map, create 2d char array
+  int w,h;
+  sscanf(buff, "%d %d", w, h);
+  char mapArray[w][h];
+
+  // fill 2d char array
+
+
+  // set 
+  &mapArray = map;
+}
+
 void serverLogic(int client_socket){
-  while (1) {
-    // do stuff
-  }
+  // do stuff
 }
 
 int main(int argc, char *argv[] ) {
   int listen_socket = server_setup();
+
+  char* mapPath = "map.txt";
+  if(argc>1){mapPath=argv[1];}
 
   while (1) {
     int client_socket = server_tcp_handshake(listen_socket);
