d472f5c2f9ed7f16e71d9470fccbbdbc1399745f
jidanahmed <jidanoutbox@gmail.com>
2026-01-11 23:09:54 -0500

    default map added

--- server.c
+++ server.c
@@ -1,5 +1,7 @@
 #include "networking.h"
 
+
+
 char map[20][20] = {
   {"##########"},
   {"#        #"},
@@ -13,18 +15,23 @@ char map[20][20] = {
   {"##########"}
 };
 
+char* mapPath = "map.txt";
+
+int numRows = (sizeof(map) / sizeof(map[0]));
+int numCols = sizeof(map[0]);
+
 void setMap(char * path) {
   // read mapPath into array
   FILE * mapFD = fopen(path, "r");
-  for (int i = 0; i < (sizeof(map) / sizeof(map[0])); i++) {
-    fgets(map[i], 11, mapFD);
+  for (int i = 0; i < numRows; i++) {
+    fgets(map[i], numCols+1, mapFD);
   }
 
   fclose(mapFD);
 }
 
 void printMap() {
-  for (int i = 0; i < 11; i++) {
+  for (int i = 0; i < (sizeof(map) / sizeof(map[0])); i++) {
     printf("%s\n", map[i]);
     // for (int j = 0; j < 10; j++) {
     //   printf("%s", map[i][j]);
@@ -34,16 +41,17 @@ void printMap() {
 
 void serverLogic(int client_socket){
   // do stuff
+
 }
 
 int main(int argc, char *argv[] ) {
-  // int listen_socket = server_setup();
-
+  int listen_socket = server_setup();
   char* mapPath = "map.txt";
   if(argc>1){mapPath=argv[1];}
 
-  // setMap(mapPath);
+  setMap(mapPath);
   printMap();
+
 
   // while (1) {
   //   int client_socket = server_tcp_handshake(listen_socket);
